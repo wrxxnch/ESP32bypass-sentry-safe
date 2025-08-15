1c7b358473aaef51e428c31903f9189ac0aabac1
Jean Pierre <73299769+wrxxnch@users.noreply.github.com>
2025-08-15 10:30:33 -0300

    Update pen-test.ino

--- pen-test/pen-test.ino
+++ pen-test/pen-test.ino
@@ -1,27 +1,40 @@
-const int pin = 1;
+#include <Arduino.h>
+
+// Pino conectado no C1
+#define PIN_C1 4  
+
+// Vamos usar Serial1 para mandar dados pelo pino C1
+// TX = PIN_C1, RX não usado (coloca -1)
+HardwareSerial C1Serial(1);
 
 void send_request(int command, int a, int b, int c, int d, int e) {
   int checksum = (command + a + b + c + d + e);
-  
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(2750);
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(200);
-  Serial.begin(4800);
 
-  Serial.write((byte)0x0);
-  Serial.write(command);
-  Serial.write(a);
-  Serial.write(b);
-  Serial.write(c);
-  Serial.write(d);
-  Serial.write(e);
-  Serial.write(checksum);
-  
-  Serial.end();
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, HIGH);
+  // Pulso inicial
+  pinMode(PIN_C1, OUTPUT);
+  digitalWrite(PIN_C1, LOW);
+  delayMicroseconds(2750);
+  digitalWrite(PIN_C1, HIGH);
+  delayMicroseconds(200);
+
+  // Inicia serial a 4800 baud no pino C1
+  C1Serial.begin(4800, SERIAL_8N1, -1, PIN_C1);
+
+  // Envia dados
+  C1Serial.write((byte)0x00);
+  C1Serial.write(command);
+  C1Serial.write(a);
+  C1Serial.write(b);
+  C1Serial.write(c);
+  C1Serial.write(d);
+  C1Serial.write(e);
+  C1Serial.write(checksum);
+  C1Serial.flush();
+  C1Serial.end();
+
+  // Mantém pino alto no final
+  pinMode(PIN_C1, OUTPUT);
+  digitalWrite(PIN_C1, HIGH);
 }
 
 void reset_code(int a, int b, int c, int d, int e) {
@@ -33,9 +46,12 @@ void try_code(int a, int b, int c, int d, int e) {
 }
 
 void setup() {
-  reset_code(1,2,3,4,5);
+  // Exemplo: definir código 1-2-3-4-5 e tentar abrir
+  reset_code(1, 2, 3, 4, 5);
   delay(1000);
-  try_code(1,2,3,4,5);
+  try_code(1, 2, 3, 4, 5);
 }
 
-void loop() {}
+void loop() {
+  // Não faz nada no loop
+}
